6ecb2775d53018b00c41ce7433b4b8ecf297faab
Gareth Woodham <garethwoodham@gmail.com>
2015-10-04 16:52:55 +0200

    Musical scale

--- beep/beep.ino
+++ beep/beep.ino
@@ -1,6 +1,20 @@
+#define NOTE_C4 115
+#define NOTE_D4 102
+#define NOTE_E4 91
+#define NOTE_F4 86
+#define NOTE_G4 77
+#define NOTE_A4 68
+#define NOTE_B4 61
+#define NOTE_C5 57
+
+boolean playTone = false;
 boolean cycleHigh = false;
-int halfCycle = 0;
-boolean beep = false;
+
+long timerCounter = 0;
+long halfCycleCounter = 0;
+
+long totalHalfCycles = 0;
+int tonePitch = 0;
 
 void setup(void) {
 
@@ -21,20 +35,65 @@ void setup(void) {
 }
 
 void loop(void) {
-	beep = !beep;
+	playBeepTone(NOTE_C4, 100);
+	delay(1000);
+	playBeepTone(NOTE_D4, 90);
+	delay(1000);
+	playBeepTone(NOTE_E4, 80);
+	delay(1000);
+	playBeepTone(NOTE_F4, 70);
+	delay(1000);
+	playBeepTone(NOTE_G4, 60);
+	delay(1000);
+	playBeepTone(NOTE_A4, 50);
+	delay(1000);
+	playBeepTone(NOTE_B4, 40);
+	delay(1000);
+	playBeepTone(NOTE_C5, 30);
+	delay(1000);
+	playBeepTone(NOTE_B4, 20);
+	delay(1000);
+	playBeepTone(NOTE_A4, 10);
+	delay(1000);
+	playBeepTone(NOTE_G4, 5);
+	delay(1000);
+	playBeepTone(NOTE_F4, 4);
+	delay(1000);
+	playBeepTone(NOTE_E4, 3);
+	delay(1000);
+	playBeepTone(NOTE_D4, 2);
+	delay(1000);
+	playBeepTone(NOTE_C4, 1);
 	delay(1000);
 }
 
+void playBeepTone(int pitch, int halfCycles) {
+	timerCounter = 0;
+	playTone = true;
+	tonePitch = pitch;
+	totalHalfCycles = halfCycles;
+}
+
 ISR(TIMER1_COMPA_vect) {
-	if (beep) {
-		if (halfCycle++ > 100) {
+	if (playTone) {
+	
+		//toggle output after n timer interrupts to create square wave
+		if (timerCounter++ > tonePitch) {
 			if (cycleHigh) {
 				OCR2B = 0;
 			} else {
 				OCR2B = 255;
 			}
 			cycleHigh = !cycleHigh;
-			halfCycle = 0;
+			timerCounter = 0;
+			halfCycleCounter++;
+		}
+		
+		//switch output off if duration exceeded
+		if (halfCycleCounter > totalHalfCycles) {
+			playTone = false;
+			halfCycleCounter = 0;
+			OCR2B = 0;
 		}
 	}
 }
